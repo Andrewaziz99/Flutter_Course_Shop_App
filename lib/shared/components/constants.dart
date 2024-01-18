d8b23a3b88499d6fe4429b5bb79b405f54d6bcd9
Andrewaziz99 <andrewaziz097@gmail.com>
2024-01-18 19:49:58 +0200

    Add new func

---
+++ lib/shared/components/constants.dart
@@ -0,0 +1,18 @@
+import 'package:souqy/modules/login/login_screen.dart';
+import 'package:souqy/shared/components/components.dart';
+import 'package:souqy/shared/network/local/cache_helper.dart';
+
+void signOut(context) {
+  CacheHelper.removeData(key: 'token').then((value) {
+    if (value) {
+      navigateAndFinish(context, ShopLoginScreen());
+    }
+  });
+}
+
+void printFullText(String text) {
+  final pattern = RegExp('.{1,800}'); // 800 is the size of each chunk
+  pattern.allMatches(text).forEach((match) => print(match.group(0)));
+}
+
+String TOKEN = '';
