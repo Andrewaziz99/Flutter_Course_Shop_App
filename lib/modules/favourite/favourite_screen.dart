a73242e559946029f6eb3c006f7fe9ba86db7c6d
Andrewaziz99 <andrewaziz097@gmail.com>
2024-01-18 19:47:43 +0200

    Add favorite screen

---
+++ lib/modules/favourite/favourite_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class FavouriteScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'Favourite Screen',
+        style: Theme.of(context).textTheme.bodyMedium,
+      ),
+    );
+  }
+}
