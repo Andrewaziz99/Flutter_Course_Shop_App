471e5c4e62e1d67e55beeb283100dc8eb4eceffa
Andrewaziz99 <andrewaziz097@gmail.com>
2024-01-18 19:48:22 +0200

    Update api handler

--- lib/shared/network/remote/dio_helper.dart
+++ lib/shared/network/remote/dio_helper.dart
@@ -8,10 +8,6 @@ class DioHelper {
       BaseOptions(
         baseUrl: 'https://student.valuxapps.com/api/',
         receiveDataWhenStatusError: true,
-        headers: {
-          'lang': 'ar',
-          'Content-Type': 'application/json',
-        },
         connectTimeout: const Duration(seconds: 30),
         receiveTimeout: const Duration(seconds: 30),
       ),
@@ -20,8 +16,8 @@ class DioHelper {
 
   static Future<Response> getData({
     required String url,
-    required Map<String, dynamic> query,
-    String lang = 'ar',
+    Map<String, dynamic>? query,
+    String lang = 'en',
     String? token,
   }) async
   {
@@ -38,7 +34,7 @@ class DioHelper {
     required String url,
     required Map<String, dynamic> data,
     Map<String, dynamic>? query,
-    String lang = 'ar',
+    String lang = 'en',
     String? token,
   }) async
   {
