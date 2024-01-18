7e48160014a3f3489e82755e8262b7fecee67b31
Andrewaziz99 <andrewaziz097@gmail.com>
2024-01-18 19:49:06 +0200

    Update layout

--- lib/layout/shop_layout.dart
+++ lib/layout/shop_layout.dart
@@ -1,25 +1,60 @@
 import 'package:flutter/material.dart';
-import 'package:souqy/modules/login/login_screen.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:souqy/modules/search/search_screen.dart';
 import 'package:souqy/shared/components/components.dart';
-import 'package:souqy/shared/network/local/cache_helper.dart';
+import 'package:souqy/shared/cubit/cubit.dart';
+import 'package:souqy/shared/cubit/states.dart';
 
 class ShopLayout extends StatelessWidget {
 
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Shop Layout'),
-      ),
-      body: TextButton(
-        onPressed: () {
-          CacheHelper.removeData(key: 'token').then((value) {
-            if (value) {
-              navigateAndFinish(context, ShopLoginScreen());
-            }
-          });
-        },
-        child: const Text('Logout')
-      ),);
+    return BlocConsumer<ShopCubit, ShopStates>(
+      listener: (BuildContext context, state) {  },
+      builder: (BuildContext context, Object? state) {
+
+        var cubit = ShopCubit.get(context);
+
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text('Souqy'),
+            actions: [
+              IconButton(
+                onPressed: ()
+                {
+                  navigateTo(context, SearchScreen());
+                },
+                icon: const Icon(Icons.search),
+              ),],
+          ),
+          body: cubit.screens[cubit.currentIndex],
+          bottomNavigationBar: BottomNavigationBar(
+            currentIndex: cubit.currentIndex,
+            onTap: (index) {
+              cubit.changeBottomNavBar(index);
+            },
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: 'Home',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.category),
+                label: 'Categories',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.favorite),
+                label: 'Favourite',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.settings),
+                label: 'Settings',
+              ),
+            ],
+          ),
+        );
+      },
+    );
   }
 }
