8e5fb0a5a299e8cdee38bd058aa839be1160e219
Andrewaziz99 <andrewaziz097@gmail.com>
2024-01-18 19:46:40 +0200

    Add products screen

---
+++ lib/modules/products/products_screen.dart
@@ -0,0 +1,230 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:conditional_builder_null_safety/conditional_builder_null_safety.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:souqy/models/categories_model.dart';
+import 'package:souqy/models/home_model.dart';
+import 'package:souqy/shared/cubit/cubit.dart';
+import 'package:souqy/shared/cubit/states.dart';
+import 'package:souqy/shared/styles/colors.dart';
+
+class ProductsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<ShopCubit, ShopStates>(
+        listener: (context, state) {},
+        builder: (context, state) {
+          return ConditionalBuilder(
+              condition: ShopCubit.get(context).homeModel != null && ShopCubit.get(context).categoriesModel != null,
+              builder: (context) =>
+                  builderWidget(ShopCubit.get(context).homeModel!, ShopCubit.get(context).categoriesModel!),
+              fallback: (context) =>
+                  const Center(child: CircularProgressIndicator()));
+        });
+  }
+
+  Widget builderWidget(HomeModel model, CategoriesModel catModel) => SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            CarouselSlider(
+              items: model.data!.banners
+                  .map((e) => Image(
+                        image: NetworkImage('${e.image}'),
+                        width: double.infinity,
+                        fit: BoxFit.cover,
+                      ))
+                  .toList(),
+              options: CarouselOptions(
+                height: 250.0,
+                initialPage: 0,
+                viewportFraction: 1.0,
+                enableInfiniteScroll: true,
+                reverse: false,
+                autoPlay: true,
+                autoPlayInterval: const Duration(seconds: 3),
+                autoPlayAnimationDuration: const Duration(milliseconds: 800),
+                autoPlayCurve: Curves.fastOutSlowIn,
+                scrollDirection: Axis.horizontal,
+              ),
+            ),
+            const SizedBox(
+              height: 10.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Categories',
+                    style: TextStyle(
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.w800,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10.0,
+                  ),
+                  Container(
+                    height: 100.0,
+                    child: ListView.separated(
+                      physics: const BouncingScrollPhysics(),
+                      shrinkWrap: true,
+                      scrollDirection: Axis.horizontal,
+                      itemBuilder: (context, index) => buildCategoryItem(catModel.data!.data[index]),
+                      separatorBuilder: (context, index) => const SizedBox(
+                        width: 10.0,
+                      ),
+                      itemCount: catModel.data!.data.length,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20.0,
+                  ),
+                  const Text(
+                    'New Products',
+                    style: TextStyle(
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.w800,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 10.0,
+            ),
+            Container(
+              color: Colors.grey[300],
+              child: GridView.count(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                crossAxisCount: 2,
+                mainAxisSpacing: 10.0,
+                crossAxisSpacing: 10.0,
+                childAspectRatio: 1 / 1.58,
+                children: List.generate(
+                  model.data!.products.length,
+                  (index) => buildGridProduct(model.data!.products[index]),
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+
+  Widget buildCategoryItem(DataModel model) => Stack(
+        alignment: AlignmentDirectional.bottomCenter,
+        children: [
+          Image(
+            image: NetworkImage('${model.image}'),
+            width: 100.0,
+            height: 100.0,
+            fit: BoxFit.cover,
+          ),
+          Container(
+            color: Colors.black.withOpacity(0.8),
+            width: 100.0,
+            child: Text(
+              '${model.name}',
+              textAlign: TextAlign.center,
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+              style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 14.0,
+                  fontWeight: FontWeight.w600
+              ),
+            ),
+          ),
+        ],
+      );
+
+  Widget buildGridProduct(ProductModel product) => Container(
+        color: Colors.white,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Stack(
+              alignment: AlignmentDirectional.bottomStart,
+              children: [
+                Image(
+                  image: NetworkImage(
+                    '${product.image}',
+                  ),
+                  width: double.infinity,
+                  height: 200.0,
+                ),
+                if (product.discount != 0)
+                  Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 5.0),
+                    color: Colors.red,
+                    child: Text(
+                      '${product.discount} % OFF',
+                      style: const TextStyle(
+                        fontSize: 10.0,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+              ],
+            ),
+            const SizedBox(
+              height: 10.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    '${product.name}',
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                    style: const TextStyle(
+                      fontSize: 14.0,
+                      height: 1.3,
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        '${product.price.round()}',
+                        style: const TextStyle(
+                          fontSize: 12.0,
+                          height: 1.3,
+                          color: DefaultColor,
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 5.0,
+                      ),
+                      if (product.discount != 0)
+                        Text(
+                          '${product.oldPrice.round()}',
+                          style: const TextStyle(
+                            fontSize: 10.0,
+                            color: Colors.grey,
+                            decoration: TextDecoration.lineThrough,
+                          ),
+                        ),
+                      const Spacer(),
+                      IconButton(
+                        padding: EdgeInsets.zero,
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.favorite_border,
+                          size: 14.0,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      );
+}
