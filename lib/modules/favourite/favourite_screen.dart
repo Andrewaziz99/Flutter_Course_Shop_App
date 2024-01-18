8c16dfc02a739059c2114a3b47d16f2058a2c27e
Andrewaziz99 <andrewaziz097@gmail.com>
2024-01-19 01:23:18 +0200

    Add Favourite Screen

--- lib/modules/favourite/favourite_screen.dart
+++ lib/modules/favourite/favourite_screen.dart
@@ -1,13 +1,130 @@
+import 'package:conditional_builder_null_safety/conditional_builder_null_safety.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:souqy/models/favorite_model.dart';
+import 'package:souqy/shared/components/components.dart';
+import 'package:souqy/shared/cubit/cubit.dart';
+import 'package:souqy/shared/cubit/states.dart';
+import 'package:souqy/shared/styles/colors.dart';
 
 class FavouriteScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Favourite Screen',
-        style: Theme.of(context).textTheme.bodyMedium,
-      ),
-    );
+    return BlocConsumer<ShopCubit, ShopStates>(
+        listener: (context, state) {},
+        builder: (context, state) {
+          return ConditionalBuilder(
+            condition: state is! ShopLoadingGetFavouritesState,
+            builder: (BuildContext context) => ListView.separated(
+              itemBuilder: (context, index) => buildFavItem(
+                  ShopCubit.get(context).favoriteModel!.data!.data![index],
+                  context),
+              separatorBuilder: (context, index) => myDivider(),
+              itemCount: ShopCubit.get(context).favoriteModel!.data!.data!.length,
+            ),
+            fallback: (BuildContext context) => const Center(
+              child: CircularProgressIndicator(),
+            )
+          );
+        });
   }
+
+  Widget buildFavItem(FavoritesData model, context) => Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Container(
+          height: 120.0,
+          child: Row(
+            children: [
+              Stack(
+                alignment: AlignmentDirectional.bottomStart,
+                children: [
+                  Image(
+                    image: NetworkImage(
+                      '${model.product?.image}',
+                    ),
+                    width: 120.0,
+                    height: 120.0,
+                  ),
+                  if (model.product?.discount != 0)
+                    Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 5.0),
+                      color: Colors.red,
+                      child: Text(
+                        '${model.product?.discount} % OFF',
+                        style: const TextStyle(
+                          fontSize: 10.0,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                ],
+              ),
+              const SizedBox(
+                width: 20.0,
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      '${model.product?.name}',
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(
+                        fontSize: 14.0,
+                        height: 1.3,
+                      ),
+                    ),
+                    const Spacer(),
+                    Row(
+                      children: [
+                        Text(
+                          '${model.product?.price.round()}',
+                          style: const TextStyle(
+                            fontSize: 12.0,
+                            height: 1.3,
+                            color: DefaultColor,
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 5.0,
+                        ),
+                        if (model.product?.discount != 0)
+                          Text(
+                            '${model.product?.oldPrice.round()}',
+                            style: const TextStyle(
+                              fontSize: 10.0,
+                              color: Colors.grey,
+                              decoration: TextDecoration.lineThrough,
+                            ),
+                          ),
+                        const Spacer(),
+                        IconButton(
+                          padding: EdgeInsets.zero,
+                          onPressed: () {
+                            ShopCubit.get(context)
+                                .changeFavourites(model.product!.id!);
+                          },
+                          icon: CircleAvatar(
+                            radius: 14.0,
+                            backgroundColor: ShopCubit.get(context)
+                                    .favourites[model.product!.id]!
+                                ? DefaultColor
+                                : Colors.grey[300],
+                            child: const Icon(
+                              Icons.favorite_border,
+                              size: 14.0,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
 }
--- lib/shared/cubit/cubit.dart
+++ lib/shared/cubit/cubit.dart
@@ -2,6 +2,8 @@ import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:souqy/models/categories_model.dart';
+import 'package:souqy/models/change_favorite_model.dart';
+import 'package:souqy/models/favorite_model.dart';
 import 'package:souqy/models/home_model.dart';
 import 'package:souqy/modules/categories/categories_screen.dart';
 import 'package:souqy/modules/favourite/favourite_screen.dart';
@@ -38,6 +40,8 @@ class ShopCubit extends Cubit<ShopStates> {
 
   HomeModel? homeModel;
 
+  Map<int, bool> favourites = {};
+
   void getHomeData() {
     emit(ShopLoadingHomeDataState());
     DioHelper.getData(
@@ -45,6 +49,9 @@ class ShopCubit extends Cubit<ShopStates> {
       token: TOKEN,
     ).then((value) {
       homeModel = HomeModel.fromJson(value.data);
+      homeModel!.data!.products.forEach((element) {
+        favourites.addAll({element.id!: element.inFavorites!});
+      });
       emit(ShopSuccessHomeDataState());
     }).catchError((error) {
       print(error.toString());
@@ -52,7 +59,7 @@ class ShopCubit extends Cubit<ShopStates> {
     });
   }
 
-CategoriesModel? categoriesModel;
+  CategoriesModel? categoriesModel;
 
   void getCatData() {
     DioHelper.getData(
@@ -63,6 +70,53 @@ CategoriesModel? categoriesModel;
     }).catchError((error) {
       print(error.toString());
       emit(ShopErrorCategoriesState());
+    });
+  }
+
+  ChangeFavoriteModel? changeFavoriteModel;
+
+  void changeFavourites(int productId) {
+
+    favourites[productId] = !favourites[productId]!;
+    emit(ShopChangeFavouritesState());
+
+    DioHelper.postData(
+      url: FAVORITES,
+      token: TOKEN,
+      data: {
+        'product_id': productId,
+      },
+    ).then((value) {
+      changeFavoriteModel = ChangeFavoriteModel.fromJson(value.data);
+      if (!changeFavoriteModel!.status!) {
+        favourites[productId] = !favourites[productId]!;
+      }else {
+        getFavourites();
+      }
+
+      emit(ShopSuccessChangeFavouritesState(changeFavoriteModel!));
+    }).catchError((error) {
+      favourites[productId] = !favourites[productId]!;
+      print(error.toString());
+      emit(ShopErrorChangeFavouritesState());
+    });
+  }
+
+  FavoriteModel? favoriteModel;
+
+  void getFavourites() {
+    emit(ShopLoadingGetFavouritesState());
+
+    DioHelper.getData(
+      url: FAVORITES,
+      token: TOKEN,
+    ).then((value) {
+      favoriteModel = FavoriteModel.fromJson(value.data);
+      printFullText(value.data.toString());
+      emit(ShopSuccessGetFavouritesState());
+    }).catchError((error) {
+      print(error.toString());
+      emit(ShopErrorGetFavouritesState());
     });
   }
 
