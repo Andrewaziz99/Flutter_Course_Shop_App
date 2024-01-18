136199e62f648ddca4153de2e51a2391b7c33146
Andrewaziz99 <andrewaziz097@gmail.com>
2024-01-18 19:46:30 +0200

    Add categories screen

---
+++ lib/modules/categories/categories_screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:souqy/models/categories_model.dart';
+import 'package:souqy/shared/components/components.dart';
+import 'package:souqy/shared/cubit/cubit.dart';
+import 'package:souqy/shared/cubit/states.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<ShopCubit, ShopStates>(
+        listener: (context, state) {},
+        builder: (context, state) {
+          return ListView.separated(
+            physics: const BouncingScrollPhysics(),
+            itemBuilder: (context, index) => buildCatItem(
+                ShopCubit.get(context).categoriesModel!.data!.data[index]),
+            separatorBuilder: (context, index) => myDivider(),
+            itemCount:
+                ShopCubit.get(context).categoriesModel!.data!.data.length,
+          );
+        });
+  }
+
+  Widget buildCatItem(DataModel model) => Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Row(
+          children: [
+            Image(
+              image: NetworkImage('${model.image}'),
+              width: 100.0,
+              height: 100.0,
+              fit: BoxFit.cover,
+            ),
+            const SizedBox(
+              width: 20.0,
+            ),
+            Text(
+              '${model.name}',
+              style: const TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const Spacer(),
+            const Icon(
+              Icons.arrow_forward_ios,
+            ),
+          ],
+        ),
+      );
+}
