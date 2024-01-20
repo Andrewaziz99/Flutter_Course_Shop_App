e1b893b23e00be97826269b73d5df125caccd597
Andrewaziz99 <andrewaziz097@gmail.com>
2024-01-21 01:34:49 +0200

    Update Screen

--- lib/modules/search/search_screen.dart
+++ lib/modules/search/search_screen.dart
@@ -1,16 +1,152 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:souqy/modules/search/cubit/cubit.dart';
+import 'package:souqy/modules/search/cubit/states.dart';
+import 'package:souqy/shared/components/components.dart';
+import 'package:souqy/shared/cubit/cubit.dart';
+import 'package:souqy/shared/styles/colors.dart';
 
 class SearchScreen extends StatelessWidget {
+  var controller = TextEditingController();
+  var formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: Text(
-          'Search Screen',
-          style: Theme.of(context).textTheme.bodyMedium,
-        ),
+    return BlocProvider(
+      create: (BuildContext context) => SearchCubit(),
+      child: BlocConsumer<SearchCubit, SearchStates>(
+        listener: (BuildContext context, state) {},
+        builder: (BuildContext context, state) {
+          return Scaffold(
+            appBar: AppBar(),
+            body: Form(
+              key: formKey,
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  children: [
+                    defaultFormField(
+                        controller: controller,
+                        type: TextInputType.text,
+                        label: 'Search',
+                        prefix: Icons.search,
+                        validate: (String? value) {
+                          if (value!.isEmpty) {
+                            return 'Enter text to search';
+                          }
+                        },
+                        onSubmit: (String text) {
+                          SearchCubit.get(context).search(text);
+                        }),
+                    const SizedBox(
+                      height: 10.0,
+                    ),
+                    if (state is SearchLoadingState)
+                      const LinearProgressIndicator(),
+                    const SizedBox(
+                      height: 10.0,
+                    ),
+                    if (state is SearchSuccessState)
+                      Expanded(
+                        child: ListView.separated(
+                          itemBuilder: (context, index) => buildSearchList(
+                              SearchCubit.get(context)
+                                  .searchModel!
+                                  .data!
+                                  .data![index],
+                              context),
+                          separatorBuilder: (context, index) => myDivider(),
+                          itemCount: SearchCubit.get(context)
+                              .searchModel!
+                              .data!
+                              .data!
+                              .length,
+                        ),
+                      )
+                  ],
+                ),
+              ),
+            ),
+          );
+        },
       ),
     );
   }
+
+  Widget buildSearchList(model, context) => Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Container(
+          height: 120.0,
+          child: Row(
+            children: [
+              Stack(
+                alignment: AlignmentDirectional.bottomStart,
+                children: [
+                  Image(
+                    image: NetworkImage(
+                      '${model.image}',
+                    ),
+                    width: 120.0,
+                    height: 120.0,
+                  ),
+                ],
+              ),
+              const SizedBox(
+                width: 20.0,
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      '${model.name}',
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(
+                        fontSize: 14.0,
+                        height: 1.3,
+                      ),
+                    ),
+                    const Spacer(),
+                    Row(
+                      children: [
+                        Text(
+                          '${model.price.round()}',
+                          style: const TextStyle(
+                            fontSize: 12.0,
+                            height: 1.3,
+                            color: DefaultColor,
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 5.0,
+                        ),
+                        const Spacer(),
+                        IconButton(
+                          padding: EdgeInsets.zero,
+                          onPressed: () {
+                            ShopCubit.get(context).changeFavourites(model.id!, context);
+                          },
+                          icon: CircleAvatar(
+                            radius: 14.0,
+                            backgroundColor:
+                                ShopCubit.get(context).favourites[model.id]?? false
+                                    ? DefaultColor
+                                    : Colors.grey[300],
+                            child: const Icon(
+                              Icons.favorite_border,
+                              size: 14.0,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
 }
